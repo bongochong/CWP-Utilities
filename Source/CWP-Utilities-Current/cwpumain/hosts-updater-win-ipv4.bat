bc3e3c91d5f200e5e0dbf5fae9372c96ed446ea4
Chongo Bong <bongochong@users.noreply.github.com>
2019-06-21 12:54:54 -0600

    added new source

--- Source/CWP-Utilities-Current/cwpumain/hosts-updater-win-ipv4.bat
+++ Source/CWP-Utilities-Current/cwpumain/hosts-updater-win-ipv4.bat
@@ -27,6 +27,7 @@ wget -nv -O processing/hosts/hosts.5 "https://hosts-file.net/exp.txt"
 wget -nv -O processing/hosts/hosts.6 "https://adaway.org/hosts.txt"
 wget -nv -O processing/hosts/hosts.7 "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/WindowsTelemetryBlockSupplements/SBBTYZ-IPv4.txt"
 wget -nv -O processing/hosts/hosts.8 "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/NoFormatting/Device9domains-IPv4.txt"
+wget -nv -O processing/hosts/hosts.9 "https://1hos.cf/mini/"
 cat processing/hosts/hosts.* > processing/hosts/hosts-cat.final
 d2u processing/hosts/hosts-cat.final
 pcregrep -v -f hostpatterns.dat processing/hosts/hosts-cat.final > processing/hosts/hosts-pre.final
