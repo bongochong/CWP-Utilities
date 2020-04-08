dfe68d2aff595ee9fe7bc62f06dc7e0787c9a036
bongochong <arbonarbot@gmail.com>
2020-04-08 01:47:32 -0600

    updated new update script

--- Source/CWP-Utilities-Current/cwpumain/update-scripts.bat
+++ Source/CWP-Utilities-Current/cwpumain/update-scripts.bat
@@ -12,6 +12,24 @@ wget -nv -O update-bt-bl-p2p.bat "https://raw.githubusercontent.com/bongochong/C
 wget -nv -O update-pac-win-local.bat "https://raw.githubusercontent.com/bongochong/CWP-Utilities/master/Source/CWP-Utilities-Current/cwpumain/update-pac-win-local.bat"
 wget -nv -O update-pac-win-template.bat "https://raw.githubusercontent.com/bongochong/CWP-Utilities/master/Source/CWP-Utilities-Current/cwpumain/update-pac-win-template.bat"
 wget -nv -O bin/hostpatterns.dat "https://raw.githubusercontent.com/bongochong/CWP-Utilities/master/Source/CWP-Utilities-Current/cwpumain/bin/hostpatterns.dat"
-echo Finished fetching scripts. You may hit enter to exit.
+echo Finished fetching scripts.
+:parse
+if "%~1"=="" goto endparse
+if "%~1"=="-el" goto endloop
+SHIFT
+goto parse
+:endparse
+echo ...
+echo Hit enter to return to the main menu.
+echo ...
+pause
+cd %~dp0
+cd ..
+WinUtilsMultiLauncher.bat
+
+:endloop
+echo ...
+echo Hit enter to exit.
+echo ...
 pause
 exit
