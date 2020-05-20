fc973f294b44097a576a2f0fc4f36f661595e6e3
bongochong <arbonarbot@gmail.com>
2020-05-20 04:16:55 -0600

    added upper-to-lower case conversion routine to hosts file scripts

--- Source/CWP-Utilities-Current/cwpumain/hosts-updater-win-dual.bat
+++ Source/CWP-Utilities-Current/cwpumain/hosts-updater-win-dual.bat
@@ -60,6 +60,7 @@ sed -i "/:/d" processing/hosts/uniq-hosts.final
 sed -i "/~/d" processing/hosts/uniq-hosts.final
 sed -i "/,/d" processing/hosts/uniq-hosts.final
 sed -i "/=/d" processing/hosts/uniq-hosts.final
+sed -i -e "s/\(.*\)/\L\1/" processing/hosts/uniq-hosts.final
 pcregrep -v -f hostpatterns.dat processing/hosts/uniq-hosts.final > processing/hosts/uniq-hosts-temp.final
 mv -f processing/hosts/uniq-hosts-temp.final processing/hosts/uniq-hosts.final
 d2u processing/hosts/uniq-hosts.final
